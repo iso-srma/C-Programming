938498437f9f457999e9318df3180b6207c2f602
Ishwor Sharma <isosrma@gmail.com>
2025-03-10 21:25:00 +0545

    Add files via upload

---
+++ Basic c program/add two integer/ishwor.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x,y,sum;
+    printf("Enter first value : ");
+    scanf("%d",&x);
+    printf("Enter second value : ");
+    scanf("%d",&y);
+    sum=x+y;
+    printf("Sum of two value is : %d",sum);
+    return 0;
+}
