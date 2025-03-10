9e30c7506854ffb45e699589108e06e41f2337a5
iso-srma <isosrma@gmail.com>
2025-03-10 20:16:03 +0545

    add more

--- printinganintegervalue.c
+++ printinganintegervalue.c
@@ -1,6 +1,17 @@
-#include<stdio.h>
-int main(){
-    int x=37;
-    printf("Printing integer value %d",x);
+// #include<stdio.h>
+// int main(){
+//     int x=37;
+//     printf("Printing integer value %d",x);
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter an integer value: ");
+    scanf("%d", &num); // value store garna laii use garney 
+
+    printf("Your integer value is: %d \n", num); // value print garna laii
     return 0;
 }
